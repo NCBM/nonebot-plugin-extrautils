a15336afb00a6efdc9ddd92b778a32589f5fc352
worldmozara <worldmozara@163.com>
2023-02-11 00:13:13 +0800

    Add docstring

--- nonebot_plugin_extrautils/ob11/universal.py
+++ nonebot_plugin_extrautils/ob11/universal.py
@@ -31,6 +31,16 @@ AVATAR_SIZE_BIG = 640
 
 
 def get_avatar_url(uid: Union[int, str], size: int = AVATAR_SIZE_BIG) -> str:
+    """
+    获取指定 QQ 用户头像 URL
+
+    参数：
+    - uid: 用户 QQ 号
+    - size: 头像尺寸
+      - 建议值: `AVATAR_SIZE_SMALL`, `AVATAR_SIZE_MEDIUM`, `AVATAR_SIZE_BIG`
+    
+    返回值: 图像 URL
+    """
     if size not in (AVATAR_SIZE_SMALL, AVATAR_SIZE_MEDIUM, AVATAR_SIZE_BIG):
         raise ValueError(
             f"unsupported avatar size: {size}\n\n"
@@ -45,6 +55,16 @@ def get_avatar_url(uid: Union[int, str], size: int = AVATAR_SIZE_BIG) -> str:
 async def get_avatar_bytes(
     uid: Union[int, str], size: int = AVATAR_SIZE_BIG
 ) -> bytes:
+    """
+    下载指定 QQ 用户头像
+
+    参数：
+    - uid: 用户 QQ 号
+    - size: 头像尺寸
+      - 建议值: `AVATAR_SIZE_SMALL`, `AVATAR_SIZE_MEDIUM`, `AVATAR_SIZE_BIG`
+
+    返回值: 图像字节数据
+    """
     async with httpx.AsyncClient(follow_redirects=True) as cli:
         res = await cli.get(get_avatar_url(uid, size))
         res.raise_for_status()
@@ -54,6 +74,16 @@ async def get_avatar_bytes(
 async def _get_user_name_bare(
     *, bot: Bot, uid: int, no_cache: bool = False
 ) -> str:
+    """
+    获取指定 QQ 用户昵称
+
+    参数：
+    - bot: OneBot v11 Bot 实例
+    - uid: 用户 ID （QQ 号）
+    - no_cache: 是否不使用缓存
+
+    返回值: 昵称字符串
+    """
     return str(
         (await bot.get_stranger_info(user_id=uid, no_cache=no_cache))
         ["nickname"]
@@ -63,6 +93,17 @@ async def _get_user_name_bare(
 async def _get_user_name_group(
     *, bot: Bot, gid: int, uid: int, no_cache: bool = False
 ) -> str:
+    """
+    获取指定 QQ 用户群昵称
+
+    参数：
+    - bot: OneBot v11 Bot 实例
+    - gid: 群 ID （群号）
+    - uid: 用户 ID （QQ 号）
+    - no_cache: 是否不使用缓存
+
+    返回值: 群名片/昵称字符串
+    """
     info = await bot.get_group_member_info(
         group_id=gid, user_id=uid, no_cache=no_cache
     )
@@ -72,6 +113,16 @@ async def _get_user_name_group(
 async def get_user_name_bare(
     *, bot: Bot, event: _UserEvent, no_cache: bool = False
 ) -> str:
+    """
+    获取指定 QQ 用户昵称
+
+    参数：
+    - bot: OneBot v11 Bot 实例
+    - event: 可提取用户 ID 的 OneBot v11 事件
+    - no_cache: 是否不使用缓存
+
+    返回值: 昵称字符串
+    """
     return await _get_user_name_bare(
         bot=bot, uid=event.user_id, no_cache=no_cache
     )
@@ -80,6 +131,16 @@ async def get_user_name_bare(
 async def get_user_name_group(
     *, bot: Bot, event: _GroupEvent, no_cache: bool = False
 ) -> str:
+    """
+    获取指定 QQ 用户群昵称
+
+    参数：
+    - bot: OneBot v11 Bot 实例
+    - event: 可提取用户 ID 和群 ID 的 OneBot v11 事件
+    - no_cache: 是否不使用缓存
+
+    返回值: 群名片/昵称字符串
+    """
     return await _get_user_name_group(
         bot=bot, gid=event.group_id, uid=event.user_id, no_cache=no_cache
     )
@@ -88,6 +149,16 @@ async def get_user_name_group(
 async def get_user_name(
     *, bot: Bot, event: _UserEvent, no_cache: bool = False
 ):
+    """
+    获取指定 QQ 用户所在会话的昵称
+
+    参数：
+    - bot: OneBot v11 Bot 实例
+    - event: 可提取用户 ID 的 OneBot v11 事件
+    - no_cache: 是否不使用缓存
+
+    返回值: 群名片/昵称字符串
+    """
     if not hasattr(event, "user_id"):
         raise AttributeError(
             f"event {event!r} does not include attrinute 'user_id'"
